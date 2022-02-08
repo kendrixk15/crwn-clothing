1e1f59b7b0b07fefbb35066324ae9eba0944c219
Kdx <sasiwong.kasantia@gmail.com>
2022-02-08 22:32:48 +0700

    create router and link pages and components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage';
+import { Switch, Route } from 'react-router-dom';
 
+const HatsPage = () =>(
+  <dvi>
+    <h1>Hats Page</h1>
+  </dvi>
+);
+
+const JacketsPage = () =>(
+  <dvi>
+    <h1>jackets Page</h1>
+  </dvi>
+);
+const SneakersPage = () =>(
+  <dvi>
+    <h1>Sneakers Page</h1>
+  </dvi>
+);
 
 function App() {
   return (
+    <Switch>
     <div>
-      <HomePage/>
+      <Route exact path="/" component={HomePage}/>
+      <Route path="/shop/hats" component={HatsPage}/>
+      <Route path="/shop/jackets" component={JacketsPage}/>
+      <Route path="/shop/sneakers" component={SneakersPage}/>
     </div>
+    </Switch>
   );
 }
 
