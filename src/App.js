7b75b08bbd379d945f11f47b24e9c6cbb5de8bfa
Kdx <sasiwong.kasantia@gmail.com>
2022-02-09 22:42:30 +0700

    created Header

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import './App.css';
 import HomePage from './pages/homepage/homepage';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
-    <Switch>
     <div>
-      <Route exact path="/" component={HomePage}/>
-      <Route path="/shop" component={ShopPage}/>
+      <Header/>
+      <Switch>
+        <div>
+          <Route exact path="/" component={HomePage}/>
+          <Route path="/shop" component={ShopPage}/>
+        </div>
+      </Switch>
     </div>
-    </Switch>
   );
 }
 
